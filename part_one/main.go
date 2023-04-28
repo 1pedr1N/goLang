9406284fd0127fd24210fc9f4498d892ea5e4c9c
Pedro Silva <88340056+1pedr1N@users.noreply.github.com>
2023-04-28 20:54:19 -0300

    new

--- part_one/main.go
+++ part_one/main.go
@@ -18,8 +18,23 @@ func main(){
 		accountNumber: 2323,
 		money:200.50,
 	}
+	var agnesAccount = CurrentAccount{
 
+		owner: "Agnes",
+		agencyNumber:232,
+		accountNumber: 1211,
+		money:1000.50,
+	}
+
+    fmt.Println(agnesAccount)
 	fmt.Println(peterAccount)
+
+	var crisAccount * CurrentAccount
+	crisAccount = new(CurrentAccount)
+	crisAccount.owner = "Cris"
+	crisAccount.money = 1983.50
+
+	fmt.Println(crisAccount)
 
 
 	
