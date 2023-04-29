ef61c6f738b4cd09001a872aebbc7080283dead1
Pedro Silva <88340056+1pedr1N@users.noreply.github.com>
2023-04-28 22:28:25 -0300

    :broken_heart:

---
+++ part_one/accounts/accounts.go
@@ -0,0 +1,42 @@
+package accounts
+
+type CurrentAccount struct {
+	owner string
+	agencyNumber int
+	accountNumber int 
+	money float64
+	}
+	
+	func (c *CurrentAccount) RetiredMoney(retiredValue float64) string {
+		canRetired :=  retiredValue > 0 && retiredValue <= c.money
+		if canRetired {
+			c.money -= retiredValue
+			return "Saque realizado com sucesso"
+		} else {
+			return "Dinheiro insuficiente"
+		}
+	
+	}
+	
+	
+		
+	
+	
+	func (c *CurrentAccount) AddMoney(depositValue float64) (string, float64) {
+		if depositValue > 0 {
+			c.money += depositValue
+			return "Deposito realizado com sucesso", c.money
+		} else { 
+			return "Valor do depósito menor que zero", c.money
+		}
+	}
+	func (c *CurrentAccount) Transfer(transferValue float64, destiny *CurrentAccount) bool {
+	if transferValue < c.money && transferValue > 0 {
+		c.money -= transferValue
+		destiny.AddMoney(transferValue)
+		return true
+	} else {
+		return false
+	}
+	}
+	
--- part_one/main.go
+++ part_one/main.go
@@ -1,46 +1,13 @@
 package main;
 import "fmt";
 
-type CurrentAccount struct {
-owner string
-agencyNumber int
-accountNumber int 
-money float64
-}
-
-func (c *CurrentAccount) RetiredMoney(retiredValue float64) string {
-	canRetired :=  retiredValue > 0 && retiredValue <= c.money
-	if canRetired {
-		c.money -= retiredValue
-		return "Saque realizado com sucesso"
-	} else {
-		return "Dinheiro insuficiente"
-	}
-
-}
-
-
-	
-
-
-func (c *CurrentAccount) AddMoney(depositValue float64) (string, float64) {
-    if depositValue > 0 {
-        c.money += depositValue
-        return "Deposito realizado com sucesso", c.money
-    } else { 
-        return "Valor do depósito menor que zero", c.money
-    }
-}
-
 
 func main(){
 
-silviaAccount := CurrentAccount{}
-silviaAccount.owner = "Silvia"
-
-silviaAccount.money = 900.50
-fmt.Println(silviaAccount.money)
-status, value := silviaAccount.AddMoney(350.00)
-fmt.Println(status, value)
- 
+silviaAccount := CurrentAccount{owner: "Silvia" , money:2000.00}
+gustavoAccount := CurrentAccount{owner: "Gustavo" , money:300.00}
+status := silviaAccount.Transfer(200.00 , &gustavoAccount)
+fmt.Println(status)
+fmt.Println(silviaAccount)
+fmt.Println(gustavoAccount)
 }
